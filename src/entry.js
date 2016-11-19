a27380ece0dba14b9ecf4a56255a6b229bfa26f4
Maximilian Noethe <maximilian.noethe@tu-dortmund.de>
2016-11-19 21:40:29 +0100

    Use timeout for typing

--- src/entry.js
+++ src/entry.js
@@ -3,14 +3,31 @@ import ReactDOM from 'react-dom';
 
 
 class Editor extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      typingTimeout: 2000,
+    };
+  }
+
   render() {
     return (
       <form>
-        <textarea />
-        <input type="submit" value="Submit" />
+        <textarea onChange={this.handleChange.bind(this)}/>
       </form>
     );
   }
+
+  handleChange() {
+    clearTimeout(this.state.typingTimer);
+    this.state.typingTimer = setTimeout(this.doneTyping, this.state.typingTimeout);
+    console.log("Change");
+  }
+
+  doneTyping() {
+    console.log("done typing");
+  }
+
 }
 
 ReactDOM.render(< Editor />, document.getElementById('editor'))
